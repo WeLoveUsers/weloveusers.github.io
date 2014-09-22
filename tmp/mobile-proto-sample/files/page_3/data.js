8f9130be741913746e13da728d3f406cc0300215
Florent Jaouali <florent.jaouali@gmail.com>
2014-09-22 18:57:09 +0200

    Adding Axure mobile sample

---
+++ tmp/mobile-proto-sample/files/page_3/data.js
@@ -0,0 +1,36 @@
+﻿$axure.loadCurrentPage({
+  "url":"page_3.html",
+  "generationDate":new Date(1411404920332.35),
+  "isCanvasEnabled":false,
+  "variables":["OnLoadVariable"],
+  "page":{
+    "packageId":"bf8b716f2d0f412c8cbb877e3351420d",
+    "type":"Axure:Page",
+    "name":"Page 3",
+    "notes":{
+},
+    "style":{
+      "baseStyle":"627587b6038d43cca051c114ac41ad32",
+      "pageAlignment":"near",
+      "fill":{
+        "fillType":"solid",
+        "color":0xFFFFFFFF},
+      "image":null,
+      "imageHorizontalAlignment":"near",
+      "imageVerticalAlignment":"near",
+      "imageRepeat":"auto",
+      "favicon":null,
+      "sketchFactor":"0",
+      "colorStyle":"appliedColor",
+      "fontName":"Applied Font",
+      "borderWidth":"0"},
+    "adaptiveStyles":{
+},
+    "interactionMap":{
+},
+    "diagram":{
+      "objects":[]}},
+  "masters":{
+},
+  "objectPaths":{
+}});
